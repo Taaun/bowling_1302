d9802fff722a79cfd47a3ce8e3bf9d43050fd511
Marc_Spectrum <sappakdee_t@silpakorn.edu>
2026-07-25 15:02:06 +0700

    Bowling

---
+++ Assets/Assets/Bowling.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bowling : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int ForcePower;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+            ShootBall();
+
+        if (Keyboard.current.rightArrowKey.isPressed)
+            MoveRight();
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+            MoveLeft();
+        if (Keyboard.current.upArrowKey.isPressed)
+            MoveForward();
+        if (Keyboard.current.downArrowKey.isPressed)
+            MoveBack();
+    }
+    private void ShootBall()
+    {
+        rb.AddForce(Vector3.forward * ForcePower, ForceMode.Impulse);
+    }
+
+    private void MoveRight()
+    {
+        transform.position += new Vector3(1f, 0f, 0f) * Time.deltaTime;
+    }
+
+    private void MoveLeft()
+    {
+        transform.position += new Vector3(-1f, 0f, 0f) * Time.deltaTime;
+    }
+
+    private void MoveForward()
+    {
+        transform.position += new Vector3(0f, 0f, 1f) * Time.deltaTime;
+    }
+
+    private void MoveBack()
+    {
+        transform.position += new Vector3(0f, 0f, -1f) * Time.deltaTime;
+    }
+}
